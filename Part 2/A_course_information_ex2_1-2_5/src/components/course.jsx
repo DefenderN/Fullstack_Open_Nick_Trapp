e5bb8d10996e5ec28e5bf9952db636c4195037d6
Nick Surface <nick.trapp@gmx.de>
2024-01-26 12:44:40 +0100

    Finished excercises 2.2 - 2.4

--- Part 2/A_course_information_ex2_1-2_5/src/components/course.jsx
+++ Part 2/A_course_information_ex2_1-2_5/src/components/course.jsx
@@ -3,10 +3,14 @@ import Content from "./content"
 
 const Course = ({course}) => {
     
+    const totalExercises = course.parts.reduce(
+        (accumulator, part) => accumulator + part.exercises, 0);
+    
     return(
         <div>
             <Header coursename = {course.name}/>
             <Content content = {course.parts}/>
+            <p>total of {totalExercises} exercises</p>
         </div>
     )
 }
