00cefd96dfc677270feea48a70b44a061f385bef
Nick Surface <nick.trapp@gmx.de>
2024-01-28 17:14:31 +0100

    Finished 2.7: The Phonebook Step2 By using the aray.some() method to check if an object with the same name is in the persons array and if so, an alert is displayed in the browser

--- Part 2/the_phonebook/src/App.jsx
+++ Part 2/the_phonebook/src/App.jsx
@@ -3,26 +3,38 @@ import PersonEntry from './components/PersonEntry'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Heiko Mikoschokolaidschaschlik' }
+    { name: 'Heiko' }
   ]) 
   const [newName, setNewName] = useState('')
 
   const addName = (event) => {
     event.preventDefault()
-
+    // Check if name is already in the persons array
+    if (personNameAlreadyExists(newName.trim(),persons)){
+    console.log(newName, "already exists in persons array") 
+    alert(`${newName} already exists in the phonebook`)
+    }
+    else {
+    console.log(newName, "will be added to the persons array")
     // Create and add new person object
     const newPersonObject = {
-      name : newName
+      name : newName.trim()
     }
     setPersons(persons.concat(newPersonObject))
-
     //reset input field
     setNewName("")
+  }
   }
 
   const handleOnChange = (event) => {
     console.log(event.target.value)
     setNewName(event.target.value)
+  }
+
+  const personNameAlreadyExists = (newPersonName, personsArray) => {
+    // TODO: Compare personstrings and return flase or true
+    return personsArray.some((person) => person.name === newPersonName)
+
   }
 
   return (
