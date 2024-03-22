49c760c83d1e9eb6291a69e22c2859981b0ab57d
Nick Surface <nick.trapp@gmx.de>
2024-03-22 08:45:26 +0100

    Added a vite proxy server to ensure that the code works in both the development environment and the production later on.

--- Part 2/thePhonebook/src/services/PersonService.js
+++ Part 2/thePhonebook/src/services/PersonService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = 'http://localhost:3001/persons'
+const baseUrl = '/api/persons'
 
 /**
  * This function returns a promise containing the Persons array from the server
